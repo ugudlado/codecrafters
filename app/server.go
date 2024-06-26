755bf229e1c9a4e34a07f806adbc19a939817e11
Mahesh Gudladona <mahesh.g@live.com>
2024-06-26 23:43:34 +0530

    Stage 8 create file

--- app/server.go
+++ app/server.go
@@ -71,7 +71,8 @@ func handleConnection(connection net.Conn, serverOptions ServerOptions) {
 		os.Exit(1)
 	}
 
-	data := string(buffer)
+	// Remove null bytes from buffer
+	data := strings.ReplaceAll(string(buffer), "\x00", "")
 
 	responseMessage := handleRequest(data, serverOptions)
 	connection.Write([]byte(responseMessage))
@@ -84,29 +85,44 @@ func handleRequest(data string, serverOptions ServerOptions) string {
 
 	responseMessage := "HTTP/1.1 404 Not Found\r\n\r\n"
 
-	if strings.HasPrefix(actualUrl, "/files/") {
-		filePath := strings.Replace(actualUrl, "/files/", "", 1)
-		filePath = serverOptions.Directory + filePath
-		if file, err := os.Open(filePath); err == nil {
-			fileInfo, _ := file.Stat()
-			fileSize := fileInfo.Size()
-			fileContent := make([]byte, fileSize)
-			file.Read(fileContent)
-			responseMessage = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", fileSize, fileContent)
-		}
-	} else if strings.HasPrefix(actualUrl, "/echo/") {
-		echoMessage := strings.Replace(actualUrl, "/echo/", "", 1)
-		responseMessage = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoMessage), echoMessage)
-	} else if strings.HasPrefix(actualUrl, "/user-agent") {
-		for i := 1; i < len(streams); i++ {
-			if strings.HasPrefix(streams[i], "User-Agent") {
-				header := strings.Replace(streams[i], "User-Agent: ", "", 1)
-				responseMessage = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(header), header)
-				break
+	if strings.HasPrefix(streams[0], "POST") {
+		if strings.HasPrefix(actualUrl, "/files/") {
+			filePath := strings.Replace(actualUrl, "/files/", "", 1)
+			filePath = serverOptions.Directory + filePath
+			fileContent := streams[len(streams)-1]
+			file, err := os.Create(filePath)
+			if err != nil {
+				responseMessage = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
 			}
+			file.Write([]byte(fileContent))
+			file.Close()
+			responseMessage = "HTTP/1.1 201 Created\r\n\r\n"
 		}
-	} else if strings.Compare(actualUrl, "/") == 0 {
-		responseMessage = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		if strings.HasPrefix(actualUrl, "/files/") {
+			filePath := strings.Replace(actualUrl, "/files/", "", 1)
+			filePath = serverOptions.Directory + filePath
+			if file, err := os.Open(filePath); err == nil {
+				fileInfo, _ := file.Stat()
+				fileSize := fileInfo.Size()
+				fileContent := make([]byte, fileSize)
+				file.Read(fileContent)
+				responseMessage = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", fileSize, fileContent)
+			}
+		} else if strings.HasPrefix(actualUrl, "/echo/") {
+			echoMessage := strings.Replace(actualUrl, "/echo/", "", 1)
+			responseMessage = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoMessage), echoMessage)
+		} else if strings.HasPrefix(actualUrl, "/user-agent") {
+			for i := 1; i < len(streams); i++ {
+				if strings.HasPrefix(streams[i], "User-Agent") {
+					header := strings.Replace(streams[i], "User-Agent: ", "", 1)
+					responseMessage = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(header), header)
+					break
+				}
+			}
+		} else if strings.Compare(actualUrl, "/") == 0 {
+			responseMessage = "HTTP/1.1 200 OK\r\n\r\n"
+		}
 	}
 	return responseMessage
 }
